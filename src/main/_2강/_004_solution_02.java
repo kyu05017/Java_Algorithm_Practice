da8220bbd8b3a9604d04ec80747489c2d126f98e
kyu05 <kyu05017@naver.com>
2025-01-06 20:36:08 +0900

    2025-01-06 2강 정리 및 풀이

--- src/main/_2강/_004_solution_02.java
+++ src/main/_2강/_004_solution_02.java
@@ -7,7 +7,7 @@ public class _004_solution_02 {
 	public static void main(String[] args){
 		solution(new int[]{4,1,3,2});
 	}
-
+	// 내풀이
 	private static boolean solution(int[] arr){
 		boolean answer = true;
 
@@ -23,4 +23,26 @@ public class _004_solution_02 {
 		return answer;
 	}
 
+	// 강의 풀이 01 (오답) - 사유 시간복잡도가 On^2이고, 틀리는 케이스가 존재
+	private static boolean solution02(int[] arr){
+		// n : 1 ~ n (1 ~ n 개의 수)
+		int cnt = 0;
+		for(int i = 0; i < arr.length; i++){
+			for(int a : arr){
+				if(a == i)cnt++;
+			}
+			if(cnt != 1)return false;
+		}
+		return true;
+	}
+
+	// 강의 풀이 02 시간복잡도는 O(nlogn)
+	private static boolean solution03(int[] arr){
+		int[] answer = new int[arr.length];
+		for(int i = 0; i < arr.length; i++) answer[i] = i+1; // O(n)
+		// arr : [0,0,0]
+		// answer : [1,2,3]
+		Arrays.sort(arr);// O(nlogn)
+		return Arrays.equals(answer,arr); // O(n)
+	}
 }
--- src/main/_2강/_005_solution_03.java
+++ src/main/_2강/_005_solution_03.java
@@ -1,12 +1,13 @@
 package src.main._2강;
 
 import java.util.Arrays;
-
+import java.util.*;
 public class _005_solution_03 {
 
 	public static void main(String[] args){
-		solution(12345);
+		solution02(12345);
 	}
+	// 내풀이
 	private static int[] solution(long n){
 		int j = 0;
 		char[] e = String.valueOf(n).toCharArray();
@@ -16,5 +17,15 @@ public class _005_solution_03 {
 			j++;
 		}
 		return answer;
+	}
+	// 강의 풀이 List 활용
+	private static int[] solution02(long n){
+		int[] answer = {};
+		List<Integer> list = new LinkedList<>();
+		while (n < 0){
+			list.add((int)n % 10);
+			n/=10;
+		}
+		return list.stream().mapToInt(Integer::intValue).toArray();
 	}
 }
